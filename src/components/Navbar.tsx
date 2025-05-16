027df207dd922dcced91bfff13eab09578e9a5ba
farbubby <farhan.mahbub@ucf.edu>
2025-05-15 21:25:49 -0400

    Ronit is ronit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,15 @@ import { useState, useRef, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import MobileMenu from "./Mobile-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+  SheetClose,
+} from "@/components/ui/sheet";
 
 const navItems = [
   { name: "Home", link: "/" },
@@ -112,7 +120,58 @@ export default function Navbar() {
           </div>
         </div>
         <div className="flex md:hidden text-white">
-          <MobileMenu />
+          <Sheet>
+            <SheetTrigger>
+              <svg
+                className="w-8 h-8 fill-gray-400 hover:fill-white transition-colors cursor-pointer"
+                clipRule="evenodd"
+                fillRule="evenodd"
+                strokeLinejoin="round"
+                strokeMiterlimit="2"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="m11 16.745c0-.414.336-.75.75-.75h9.5c.414 0 .75.336.75.75s-.336.75-.75.75h-9.5c-.414 0-.75-.336-.75-.75zm-9-5c0-.414.336-.75.75-.75h18.5c.414 0 .75.336.75.75s-.336.75-.75.75h-18.5c-.414 0-.75-.336-.75-.75zm4-5c0-.414.336-.75.75-.75h14.5c.414 0 .75.336.75.75s-.336.75-.75.75h-14.5c-.414 0-.75-.336-.75-.75z"
+                  fillRule="nonzero"
+                />
+              </svg>
+            </SheetTrigger>
+            <SheetContent className="bg-gray-900/50 border-gray-700 backdrop-blur-md hover:shadow-[0_0_20px_rgba(15,244,198,0.15)] hover:border-[#0ff4c6]/50 transition-all duration-300">
+              <SheetHeader>
+                <SheetTitle></SheetTitle>
+                <SheetDescription></SheetDescription>
+              </SheetHeader>
+              <div className="flex flex-col gap-12 items-center justify-center p-4 h-full w-full">
+                {navItems.map((active) => (
+                  <SheetClose key={active.name}>
+                    <a
+                      href={active.link}
+                      key={active.name}
+                      className={cn(
+                        "transition-colors duration-200 py-5 px-4 cursor-pointer text-glow-subtle flex flex-col gap-2",
+                        activeItem === active.name &&
+                          "text-[#0ff4c6] hover:text-[#0ff4c6]"
+                      )}
+                      onClick={() => {
+                        setActiveItem(active.name);
+                      }}>
+                      <div
+                        className={cn(
+                          activeItem === active.name
+                            ? "text-glow duration-200"
+                            : "text-gray-300 hover:text-white duration-200"
+                        )}>
+                        {active.name}
+                      </div>
+                      {activeItem === active.name && (
+                        <div className="bottom-0 h-0.75 bg-[#0ff4c6] rounded-t-sm shadow-[0_0_8px_#0ff4c6,0_0_15px_rgba(15,244,198,0.6)] animate-pulse-subtle" />
+                      )}
+                    </a>
+                  </SheetClose>
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </nav>
