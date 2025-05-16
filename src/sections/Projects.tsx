99e7d03155762a7693eb24fd7f05050d500313fc
farbubby <farhan.mahbub@ucf.edu>
2025-05-15 21:21:11 -0400

    Womp womp

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -9,82 +9,82 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
-export default function ProjectSection() {
-  const projectList = [
-    <Project
-      key="1"
-      title="Find My Professors"
-      description="A web application that aims to help college students find their professors by searching for the course they plan to take and provides in-depth analysis and relevant data for each professor teaching it."
-      technologies={[
-        "Go",
-        "GraphQL",
-        "SQL",
-        "PostgreSQL",
-        "Nextjs",
-        "TailwindCSS",
-      ]}
-      website=""
-    />,
-    <Project
-      key="2"
-      title="Handy Dollar"
-      description="A financial planning tool application that aims to help users understand and break down their transactions and expenses in order to better manage their finances."
-      technologies={[
-        "TypeScript",
-        "Nextjs",
-        "TailwindCSS",
-        "PostgreSQL",
-        "OpenAI",
-        "Azure",
-        "Supabase",
-        "Plaid",
-      ]}
-      website=""
-    />,
-    <Project
-      key="3"
-      title="Review Summarizer"
-      description="A web application that grabs top positive and negative reviews for a product and generates a pros and cons and a general consensus summary for that product."
-      technologies={[
-        "TypeScript",
-        "Nextjs",
-        "TailwindCSS",
-        "Expressjs",
-        "Puppeteer",
-        "OpenAI",
-      ]}
-      website=""
-    />,
-    <Project
-      key="4"
-      title="Code Clash"
-      description="A leetcode style battle game where players get matched against each other to find the fastest solutions to coding problems in the shortest amount of time."
-      technologies={[
-        "TypeScript",
-        "Mithril",
-        "TailwindCSS",
-        "Expressjs",
-        "Socketio",
-        "MongoDB",
-      ]}
-      website=""
-    />,
-    <Project
-      key="5"
-      title="Overtone"
-      description="A small AI chat application which is similar to the ChatGPT but with an extra feature of text-to-speech to bring accessibility and add a more interactive experience."
-      technologies={[
-        "TypeScript",
-        "Nextjs",
-        "TailwindCSS",
-        "AWS",
-        "Go",
-        "OpenAI",
-      ]}
-      website=""
-    />,
-  ];
+const projectList = [
+  <Project
+    key="1"
+    title="Find My Professors"
+    description="A web application that aims to help college students find their professors by searching for the course they plan to take and provides in-depth analysis and relevant data for each professor teaching it."
+    technologies={[
+      "Go",
+      "GraphQL",
+      "SQL",
+      "PostgreSQL",
+      "Nextjs",
+      "TailwindCSS",
+    ]}
+    website=""
+  />,
+  <Project
+    key="2"
+    title="Handy Dollar"
+    description="A financial planning tool application that aims to help users understand and break down their transactions and expenses in order to better manage their finances."
+    technologies={[
+      "TypeScript",
+      "Nextjs",
+      "TailwindCSS",
+      "PostgreSQL",
+      "OpenAI",
+      "Azure",
+      "Supabase",
+      "Plaid",
+    ]}
+    website=""
+  />,
+  <Project
+    key="3"
+    title="Review Summarizer"
+    description="A web application that grabs top positive and negative reviews for a product and generates a pros and cons and a general consensus summary for that product."
+    technologies={[
+      "TypeScript",
+      "Nextjs",
+      "TailwindCSS",
+      "Expressjs",
+      "Puppeteer",
+      "OpenAI",
+    ]}
+    website=""
+  />,
+  <Project
+    key="4"
+    title="Code Clash"
+    description="A leetcode style battle game where players get matched against each other to find the fastest solutions to coding problems in the shortest amount of time."
+    technologies={[
+      "TypeScript",
+      "Mithril",
+      "TailwindCSS",
+      "Expressjs",
+      "Socketio",
+      "MongoDB",
+    ]}
+    website=""
+  />,
+  <Project
+    key="5"
+    title="Overtone"
+    description="A small AI chat application which is similar to the ChatGPT but with an extra feature of text-to-speech to bring accessibility and add a more interactive experience."
+    technologies={[
+      "TypeScript",
+      "Nextjs",
+      "TailwindCSS",
+      "AWS",
+      "Go",
+      "OpenAI",
+    ]}
+    website=""
+  />,
+];
 
+export default function ProjectSection() {
   return (
     <>
       <Carousel
